620c879047815808fc798c574d4d13f9df0f6a98
xiaolang124 <tiany1992@sina.com>
2017-11-11 23:24:02 +0800

    reverse str

---
+++ java/algorithm/回文字符串.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while(x!=0){
+        	int tail = x%10;
+        	int temp = res*10 + tail;
+        	if((temp-tail)/10!=res)
+        		return 0;
+        	res = temp;
+        	x /= 10;
+        }
+        return res;
+    }
+}
