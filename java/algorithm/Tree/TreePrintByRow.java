862083f0df5e1efe0ff41e349b47b814206c4b82
xiaolang124 <tiany1992@sina.com>
2017-12-04 14:52:37 +0800

    pre order and serialize

--- java/algorithm/Tree/TreePrintByRow.java
+++ java/algorithm/Tree/TreePrintByRow.java
@@ -19,7 +19,7 @@ public class TreePrintByRow {
 
         while (!temp.isEmpty()){
             now = temp.poll();
-            System.out.print(now.getData());
+            System.out.print(now.getData() + " ");
             if(now == rEnd){
                 System.out.println();
                 if(now.getLeft()!=null){
@@ -39,41 +39,5 @@ public class TreePrintByRow {
                 }
             }
         }
-    }
-}
-
-
-public class Node {
-    private int data;
-    private Node left;
-    private Node right;
-
-    public Node(int data) {
-        this.data = data;
-    }
-
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
     }
 }
